9cabac34bffd75f8f74427fa1d0297692f58ae47
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-12-23 16:54:35 +0430

    add project structre

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+// Switch is used to group routes, Route is used to define individual routes, and Link is used for navigation between routes.
+import { Layout, Typography, Space } from 'antd' 
+// Layout provides a structure for the application, Typography is used for text styling, and Space is used for spacing between elements. these are components from the Ant Design library. for more details visit https://ant.design/components/overview/
+
+function App() {
+
+  return (
+    
+      <div className="app">
+        <div className="navbar">
+
+        </div>
+        <div className="main">
+      </div>
+      <div className="footer">
+        
+      </div>
+
+        
+      </div>
+  )
+}
+
+export default App
