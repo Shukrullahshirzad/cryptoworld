679ef280c014e491415eab3543f0721e150eb8de
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-12-23 21:35:37 +0430

    add components and related routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 // Switch is used to group routes, Route is used to define individual routes, and Link is used for navigation between routes.
 import { Layout, Typography, Space } from 'antd' 
 // Layout provides a structure for the application, Typography is used for text styling, and Space is used for spacing between elements. these are components from the Ant Design library. for more details visit https://ant.design/components/overview/
-import { Navbar } from './components'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
 // this type of import is called named import. It imports the Navbar component from the components directory.
 import './App.css'
 
@@ -15,7 +15,18 @@ function App() {
         <div className="navbar">
           <Navbar />
         </div>
-        <div className="main">
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage/>} />
+              <Route path="/exchanges" element={<Exchanges/>} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies/>} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails/>} />
+              <Route path="/news" element={<News/>} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className="footer">
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-
+import 'antd/dist/antd.css'; // this will import the default Ant Design styles for the application
+// it includes styles for buttons, forms, layouts, and other UI components provided by Ant Design.
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
