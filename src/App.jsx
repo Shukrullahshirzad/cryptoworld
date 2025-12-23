f1036b8180d5e7b3baa2b835e8db84c28b440776
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-12-23 21:20:13 +0430

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 // Switch is used to group routes, Route is used to define individual routes, and Link is used for navigation between routes.
 import { Layout, Typography, Space } from 'antd' 
 // Layout provides a structure for the application, Typography is used for text styling, and Space is used for spacing between elements. these are components from the Ant Design library. for more details visit https://ant.design/components/overview/
+import { Navbar } from './components'
+// this type of import is called named import. It imports the Navbar component from the components directory.
+import './App.css'
 
 function App() {
 
@@ -10,12 +13,12 @@ function App() {
     
       <div className="app">
         <div className="navbar">
-
+          <Navbar />
         </div>
         <div className="main">
       </div>
       <div className="footer">
-        
+
       </div>
 
         
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import { createRoot } from 'react-dom/client'
+import App from "./App.jsx";
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  ,
-)
+  </BrowserRouter>
+);
