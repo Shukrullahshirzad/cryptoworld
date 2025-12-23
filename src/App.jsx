3efab4c9fb5bf584e542b000d3b04c3bd3e393f4
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-12-23 22:03:14 +0430

    add footer content

--- src/App.jsx
+++ src/App.jsx
@@ -27,9 +27,17 @@ function App() {
             </Routes>
           </div>
         </Layout>
-      </div>
-      <div className="footer">
-
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            CryptoWorld <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
 
         
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // BrowserRouter is a router implementation that uses the HTML5 history API (pushState, replaceState and the popstate event) to keep your UI in sync with the URL.
 
 import App from "./App.jsx";
 import 'antd/dist/antd.css'; // this will import the default Ant Design styles for the application
